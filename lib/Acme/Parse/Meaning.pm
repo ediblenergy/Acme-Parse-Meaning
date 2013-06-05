70183551bb63a4a56a41ee41ef3cf0f395580abe
skaufman <sam@socialflow.com>
2013-06-05 16:37:45 -0400

    it doesnt die

--- lib/Acme/Parse/Meaning.pm
+++ lib/Acme/Parse/Meaning.pm
@@ -3,6 +3,7 @@ use strict;
 use warnings FATAL => 'all';
 use Moo;
 use Marpa::R2;
+use feature 'say';
 
 has text => (
     is => 'ro',
@@ -14,16 +15,46 @@ has grammar => ( is => 'lazy' );
 has proper_nouns => ( is => 'lazy' );
 
 sub _build_grammar {
-    my $source =<<"EOS";
-    <document>          ::= paragraph+
-    <paragraph>         ::= sentence+ VERTICAL_SPACE
-    <sentence>          ::= sentence-part+ .
-    <sentence-part>     ::= word+ | proper-noun | opt-punctuation
-    <word>              ::= LETTER+
-    <opt-punctuation>   ::= PUNCTUATION_MARK | ""
-    <proper-noun>       ::= capitalized-word+
-    <capitalized-word>  ::= " " UPPER_CASE_LETTER LOWER_CASE_LETTER+ " " | PUNCTUATION_MARK
+    my $source =<<'EOS';
+:start              ::= document
+document            ::= paragraph+
+paragraph           ::= sentence+
+sentence            ::= sentence_part+
+sentence_part       ::=   words | proper_noun
+words ::= words_and_space+
+words_and_space ::= word opt_whitespace 
+opt_whitespace ~ [\h]+
+word                 ~ [\b\w+\b]
+proper_noun       ~ [\b\w+\w]
 EOS
+
+=head2 BUsted
+sentence_part       ::= word+ | proper_noun
+capitalized-word  ::= WORD_BORDER UPPER_CASE_LETTER LOWER_CASE_LETTER+ WORD_BORDER | PUNCTUATION_MARK
+:start ::= Expression
+Expression ::= Term
+Term ::=
+      Factor
+    | Term '+' Term action => do_add
+Factor ::=
+      Number
+    | Factor '*' Factor action => do_multiply
+      Number ~ digits
+      digits ~ [\d]+
+      :discard ~ whitespace
+      whitespace ~ [\s]+
+
+
+document          ::= paragraph+
+paragraph         ::= sentence+ VERTICAL_SPACE
+VERTICAL_SPACE ~  [\v]+
+sentence part     ::= word+ | proper-noun | opt-punctuation
+<word>              ::= LETTER+
+<opt-punctuation>   ::= PUNCTUATION_MARK | ""
+<proper-noun>       ::= capitalized-word+
+<capitalized-word>  ::= WORD_BORDER UPPER_CASE_LETTER LOWER_CASE_LETTER+ WORD_BORDER | PUNCTUATION_MARK
+
+=cut
 
       return Marpa::R2::Scanless::G->new({
         source         => \$source,
@@ -34,11 +65,23 @@ EOS
 }
 sub parse {
     my $self = shift;
+    warn 'before recce';
     my $recce = Marpa::R2::Scanless::R->new( { grammar => $self->grammar } );
+    warn 'after recce';
     my $content = $self->text;
+    warn "trying to parse '$content'";
     $recce->read( \$content );
     my $value_ref = $recce->value;
     my $value = $value_ref ? ${$value_ref} : 'No Parse';
+}
+
+sub MeaningNodes::new { +{} }
+sub MeaningNodes::do_add {
+    return @_;
+}
+
+sub MeaningNodes::do_multiply {
+    return @_;
 }
 
 1;
--- t/parse.t
+++ t/parse.t
@@ -1,9 +1,12 @@
 use strict;
 use warnings FATAL => 'all';
 use Test::More;
+plan tests => 1;
 use Acme::Parse::Meaning;
 use Data::Dumper::Concise;
-my $one_pn = "I'm going to New York.";
+my $one_pn = "Im going to New York";
+#my $one_pn = "5 + 6";
 my $parser = Acme::Parse::Meaning->new( text => $one_pn );
-ok $parser->parse;
+ok $parser->parse,
+  'successfully parsed';
 1;
